121c785d90415d1952c643b4382614f038ae85ac
LitoMore <litomore@gmail.com>
2020-02-12 22:10:25 +0800

    Add UI kit

--- web/src/app.js
+++ web/src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as echarts from 'echarts';
+import {Radio} from 'antd';
 import {renderOption} from './mock/options';
 import mapLayer from './geojson/qingdao';
 
@@ -16,13 +17,23 @@ class App extends React.Component {
 
 	render() {
 		return (
-			<div
-				id="main"
-				style={{
-					width: 900,
-					height: 500
-				}}
-			/>
+			<section>
+				<div>
+					<Radio.Group defaultValue="a" buttonStyle="solid">
+						<Radio.Button value="a">定点医院</Radio.Button>
+						<Radio.Button value="b">2</Radio.Button>
+						<Radio.Button value="c">3</Radio.Button>
+						<Radio.Button value="d">4</Radio.Button>
+					</Radio.Group>
+				</div>
+				<div
+					id="main"
+					style={{
+						width: 900,
+						height: 500
+					}}
+				/>
+			</section>
 		);
 	}
 }
