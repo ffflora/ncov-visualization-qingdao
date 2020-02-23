c323dd7a148f02380b6666484c36936c3c81bdaf
LitoMore <litomore@gmail.com>
2020-02-23 19:16:43 +0800

    Prevent data merge on setOption

--- web/src/app.js
+++ web/src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as echarts from 'echarts';
-import {renderOption} from './mock/options';
+import {getOption} from './mock/options';
 import mapLayer from './geojson/qingdao';
 
 class App extends React.Component {
@@ -9,12 +9,27 @@ class App extends React.Component {
 		echarts.registerMap('青岛', mapLayer);
 	}
 
+	state = {
+		mapType: 'clinic'
+	}
+
 	componentDidMount() {
-		const charts = echarts.init(document.querySelector('#main'), 'white', {renderer: 'canvas'});
-		charts.setOption(renderOption);
+		const {mapType} = this.state;
+		this.charts = echarts.init(document.querySelector('#main'), 'white', {renderer: 'canvas'});
+		this.charts.setOption(getOption(mapType));
+	}
+
+	onChangeType = e => {
+		const type = e.currentTarget.value;
+		this.setState({mapType: type}, () => {
+			this.charts.setOption(getOption(type), {notMerge: true});
+			this.charts.resize();
+		});
 	}
 
 	render() {
+		const {mapType} = this.state;
+
 		return (
 			<section>
 				<div
@@ -24,6 +39,53 @@ class App extends React.Component {
 						height: '100vh'
 					}}
 				/>
+				<div
+					style={{
+						position: 'fixed',
+						right: 100,
+						bottom: 100
+					}}
+				>
+					<div>
+						<label style={{marginRight: 5}}>
+							<input
+								checked={mapType === 'clinic'}
+								value="clinic"
+								type="radio"
+								className="nes-radio"
+								name="export-type"
+								onChange={this.onChangeType}
+							/>
+							<span>Clinic</span>
+						</label>
+					</div>
+					<div>
+						<label style={{marginRight: 5}}>
+							<input
+								checked={mapType === 'cured'}
+								value="cured"
+								type="radio"
+								className="nes-radio"
+								name="export-type"
+								onChange={this.onChangeType}
+							/>
+							<span>Cured</span>
+						</label>
+					</div>
+					<div>
+						<label style={{marginRight: 5}}>
+							<input
+								checked={mapType === 'diag'}
+								value="diag"
+								type="radio"
+								className="nes-radio"
+								name="export-type"
+								onChange={this.onChangeType}
+							/>
+							<span>Diag</span>
+						</label>
+					</div>
+				</div>
 			</section>
 		);
 	}
--- web/src/mock/options.js
+++ web/src/mock/options.js
@@ -1,140 +1,150 @@
 import clinic from '../dataset/clinic-coord';
 
-export const renderOption = {
-	animation: true,
-	animationThreshold: 2000,
-	animationDuration: 1000,
-	animationEasing: 'cubicOut',
-	animationDelay: 0,
-	animationDurationUpdate: 300,
-	animationEasingUpdate: 'cubicOut',
-	animationDelayUpdate: 0,
-	color: [
-		'#c23531',
-		'#2f4554',
-		'#61a0a8',
-		'#d48265',
-		'#749f83',
-		'#ca8622',
-		'#bda29a',
-		'#6e7074',
-		'#546570',
-		'#c4ccd3',
-		'#f05b72',
-		'#ef5b9c',
-		'#f47920',
-		'#905a3d',
-		'#fab27b',
-		'#2a5caa',
-		'#444693',
-		'#726930',
-		'#b2d235',
-		'#6d8346',
-		'#ac6767',
-		'#1d953f',
-		'#6950a1',
-		'#918597'
-	],
-	series: [
-		{
-			name: '',
-			type: 'effectScatter',
-			coordinateSystem: 'geo',
-			data: clinic.map(([key, value]) => {
-				return {
-					name: key,
-					value
-				};
-			}),
-			symbolSize() {
-				return 10;
-			},
-			showEffectOn: 'render',
-			rippleEffect: {
-				brushType: 'stroke'
-			},
-			hoverAnimation: true,
-			label: {
-				formatter: '{b}',
-				position: 'right',
-				show: false
-			},
-			itemStyle: {
-				color: 'purple',
-				shadowBlur: 10,
-				shadowColor: '#333'
-			},
-			zlevel: 1
-		}
-	],
-	// Legend: [
-	// 	{
-	// 		data: [
-	// 			'\u786E\u8BCA\u4EBA\u6570-Scatter',
-	// 			'\u786E\u8BCA\u4EBA\u6570-heatmap'
-	// 		],
-	// 		selected: {
-	// 			'\u786E\u8BCA\u4EBA\u6570-Scatter': true,
-	// 			'\u786E\u8BCA\u4EBA\u6570-heatmap': true
-	// 		},
-	// 		show: false,
-	// 		padding: 5,
-	// 		itemGap: 10,
-	// 		itemWidth: 25,
-	// 		itemHeight: 14
-	// 	}
-	// ],
-	tooltip: {
-		show: true,
-		trigger: 'item',
-		triggerOn: 'mousemove|click',
-		axisPointer: {
-			type: 'line'
-		},
-		formatter(params) {
-			return params.name;
-		},
-		textStyle: {
-			fontSize: 14
-		},
-		borderWidth: 0
-	},
-	title: [
-		{
-			text: '定点医院分布图',
-			padding: 5,
-			itemGap: 10
-		}
-	],
-	visualMap: {
-		show: false,
-		type: 'continuous',
-		min: 0,
-		max: 30,
-		inRange: {
-			color: [
-				'#50a3ba',
-				'#eac763',
-				'#d94e5d'
-			]
-		},
-		calculable: true,
-		inverse: false,
-		splitNumber: 5,
-		orient: 'vertical',
-		showLabel: true,
-		itemWidth: 20,
-		itemHeight: 140,
-		borderWidth: 0
-	},
-	geo: {
-		map: '\u9752\u5c9b',
-		roam: true,
-		itemStyle: {
-			color: ' #fff',
-			borderColor: '#222222'
-		},
-		emphasis: {}
+export const getOption = type => {
+	let series = [];
+	switch (type) {
+		case 'clinic':
+			series = [{
+				name: 'clinic',
+				type: 'effectScatter',
+				coordinateSystem: 'geo',
+				data: clinic.map(([key, value]) => {
+					return {
+						name: key,
+						value
+					};
+				}),
+				symbolSize() {
+					return 10;
+				},
+				showEffectOn: 'render',
+				rippleEffect: {
+					brushType: 'stroke'
+				},
+				hoverAnimation: true,
+				label: {
+					formatter: '{b}',
+					position: 'right',
+					show: false
+				},
+				itemStyle: {
+					color: 'purple',
+					shadowBlur: 10,
+					shadowColor: '#333'
+				},
+				zlevel: 1
+			}];
+			break;
+		default:
+			series = [];
+			break;
 	}
+
+	return {
+		animation: true,
+		animationThreshold: 2000,
+		animationDuration: 1000,
+		animationEasing: 'cubicOut',
+		animationDelay: 0,
+		animationDurationUpdate: 300,
+		animationEasingUpdate: 'cubicOut',
+		animationDelayUpdate: 0,
+		color: [
+			'#c23531',
+			'#2f4554',
+			'#61a0a8',
+			'#d48265',
+			'#749f83',
+			'#ca8622',
+			'#bda29a',
+			'#6e7074',
+			'#546570',
+			'#c4ccd3',
+			'#f05b72',
+			'#ef5b9c',
+			'#f47920',
+			'#905a3d',
+			'#fab27b',
+			'#2a5caa',
+			'#444693',
+			'#726930',
+			'#b2d235',
+			'#6d8346',
+			'#ac6767',
+			'#1d953f',
+			'#6950a1',
+			'#918597'
+		],
+		series,
+		// Legend: [
+		// 	{
+		// 		data: [
+		// 			'\u786E\u8BCA\u4EBA\u6570-Scatter',
+		// 			'\u786E\u8BCA\u4EBA\u6570-heatmap'
+		// 		],
+		// 		selected: {
+		// 			'\u786E\u8BCA\u4EBA\u6570-Scatter': true,
+		// 			'\u786E\u8BCA\u4EBA\u6570-heatmap': true
+		// 		},
+		// 		show: false,
+		// 		padding: 5,
+		// 		itemGap: 10,
+		// 		itemWidth: 25,
+		// 		itemHeight: 14
+		// 	}
+		// ],
+		tooltip: {
+			show: true,
+			trigger: 'item',
+			triggerOn: 'mousemove|click',
+			axisPointer: {
+				type: 'line'
+			},
+			formatter(params) {
+				return params.name;
+			},
+			textStyle: {
+				fontSize: 14
+			},
+			borderWidth: 0
+		},
+		title: [
+			{
+				text: '定点医院分布图',
+				padding: 5,
+				itemGap: 10
+			}
+		],
+		visualMap: {
+			show: false,
+			type: 'continuous',
+			min: 0,
+			max: 30,
+			inRange: {
+				color: [
+					'#50a3ba',
+					'#eac763',
+					'#d94e5d'
+				]
+			},
+			calculable: true,
+			inverse: false,
+			splitNumber: 5,
+			orient: 'vertical',
+			showLabel: true,
+			itemWidth: 20,
+			itemHeight: 140,
+			borderWidth: 0
+		},
+		geo: {
+			map: '\u9752\u5c9b',
+			roam: true,
+			itemStyle: {
+				color: ' #fff',
+				borderColor: '#222222'
+			},
+			emphasis: {}
+		}
+	};
 };
 
